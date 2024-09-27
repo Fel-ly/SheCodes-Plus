5768d45fcdb4f9a4f217c3c14b8afa0d0309cdc0
Fel-ly <kiokofpm@gmail.com>
2024-09-27 23:34:17 +0300

    JS Math

---
+++ Week3/JS Math/index.js
@@ -0,0 +1,16 @@
+let price = 4.5;
+
+// Challenge 1
+console.log(Math.round(price));
+
+//challenge 2
+console.log(Math.ceil(price));
+
+//challenge 3
+console.log(Math.floor(price));
+
+//challenge 4
+console.log(Math.min(4, 1, 3, 10));
+
+//challenge 5
+console.log(Math.max(4, 1, 3, 10));
